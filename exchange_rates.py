9a1875c043712e889fbc1bee24d1307b4be1dd89
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-10 14:22:27 +0000

    exchange rates pseudo code added

--- exchange_rates.py
+++ exchange_rates.py
@@ -1,13 +1,16 @@
+# import json as we are using json file for exchange rate data
 import json
 
 
 def Find_exchange_rate():
+    # copies the data and stores it to a variable "exchange rates"
     with open("exchange_rates.json", "r") as jsonfile:
         exchange_rates = json.load(jsonfile)
         print(exchange_rates)
 
+        # for loop to iterate over the nested dictionary "rates" to print keys and values
         for key in exchange_rates["rates"]:
             print(f"Currency : {key}, Exchange Rate : {exchange_rates['rates'][key]}")
 
-
+# calling the function
 Find_exchange_rate()
